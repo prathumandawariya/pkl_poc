e23590d38583ca2413c54b04ee5e077eeee4c3b7
Prathu Mandawariya <65664273+prathumandawariya@users.noreply.github.com>
2024-03-21 02:29:46 +0530

    corrected health check URL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.2.3"
@@ -17,6 +18,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.core:jackson-core:2.17.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/java/com/pkl/poc/app/controller/PklController.java
+++ src/main/java/com/pkl/poc/app/controller/PklController.java
@@ -1,19 +1,30 @@
 package com.pkl.poc.app.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 
 @RestController
 public class PklController {
     
     @GetMapping("/")
     public String home() {
-        return "Welcome to Spring Boot!";
+        return "Welcome to PKL POC";
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, world!";
+    @GetMapping("/health")
+    public ResponseEntity<JsonNode> health() throws JsonMappingException, JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree("{\"status\": \"ok\"}");
+        return ResponseEntity.ok(json);
     }
 }
 
